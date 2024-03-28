931957df951f7cc7b9b4b4b7db2849ff103a7e7c
wmmando0 <59392618+waelmando0@users.noreply.github.com>
2024-03-28 13:50:40 +0200

    add tremor dashboard

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,13 +1,16 @@
+import AnalyticsDashboard from '@/components/analytics-dashboard';
 import { analytics } from '@/utils/analytics';
 import React from 'react';
 
 const page = async () => {
-	const pageView = await analytics.retrieve('pageview', '27/3/2024');
+	// const pageView = await analytics.retrieveDays('pageview', 2);
 
 	return (
-		<div>
-			<pre>{JSON.stringify(pageView)}</pre>
-		</div>
+		<section className='min-h-screen w-full flex items-center justify-center py-4 px-6'>
+			<div className='relative w-full max-w-6xl mx-auto'>
+				<AnalyticsDashboard />
+			</div>
+		</section>
 	);
 };
 
---
+++ components/analytics-dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AnalyticsDashboard = () => {
+	return (
+		<div className='flex flex-col gap-6'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 w-full mx-auto'>
+				<p>test</p>
+				<p>test</p>
+			</div>
+		</div>
+	);
+};
+
+export default AnalyticsDashboard;
